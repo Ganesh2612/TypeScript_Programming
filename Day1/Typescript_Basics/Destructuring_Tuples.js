2ac235626a7dfe4ae706e0a5dab0e66a76fcd5b5
Ganesh <ganeshgopi2612@gmail.com>
2025-05-06 12:22:08 +0530

    [Ganesh] add : Destructing tuples

---
+++ Day1/Typescript_Basics/Destructuring_Tuples.js
@@ -0,0 +1,6 @@
+var marks = [98, 92, 88];
+marks.push(95);
+var x = marks[0], y = marks[1], z = marks[2];
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ Day1/Typescript_Basics/Destructuring_Tuples.ts
@@ -0,0 +1,7 @@
+let marks: [number,number,number] = [98,92,88];
+marks.push(95);
+let [x,y,z]=marks;
+console.log(x);
+console.log(y);
+console.log(z);
+
