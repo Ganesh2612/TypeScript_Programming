c5c76c15f3f485f6fcdd9afd139175f3fffb8a03
Ganesh <ganeshgopi2612@gmail.com>
2025-05-06 15:12:07 +0530

    [Ganesh] add : Implementation of Interfaces

---
+++ Day1/Typescript_Basics/Interfaces.js
@@ -0,0 +1,6 @@
+var coloredRectangle = {
+    height: 20,
+    width: 10,
+    color: "White"
+};
+console.log(coloredRectangle);
---
+++ Day1/Typescript_Basics/Interfaces.ts
@@ -0,0 +1,15 @@
+interface Rectangle {
+    height: number,
+    width: number
+  }
+  
+  interface ColoredRectangle extends Rectangle {
+    color: string
+  }
+  
+  const coloredRectangle: ColoredRectangle = {
+    height: 20,
+    width: 10,
+     color:"White"
+  };
+  console.log(coloredRectangle);
