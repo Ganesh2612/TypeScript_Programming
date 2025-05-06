bf8c91bc3fbd88f34cae46b9e8a4bc7d82dbd896
Ganesh <ganeshgopi2612@gmail.com>
2025-05-06 12:40:54 +0530

    [Ganesh] add : Objects in ts

---
+++ Day1/Typescript_Basics/Objects.js
@@ -0,0 +1,6 @@
+var Car = {
+    name: "BMW",
+    Model: "Ic3987"
+};
+Car.id = 34;
+console.log(Car);
---
+++ Day1/Typescript_Basics/Objects.ts
@@ -0,0 +1,6 @@
+let Car:{name:string,Model:string,id?:number}={
+       name:"BMW",
+       Model:"Ic3987"
+}
+Car.id=34;
+console.log(Car);
