78ce03da5143199fa5075a747ae0e9dfa56cdeb8
Ganesh <ganeshgopi2612@gmail.com>
2025-05-06 11:48:27 +0530

    [Ganesh] add : Implementing Arrays with different data types

---
+++ Day1/Typescript_Basics/Arrays_Different_Types.js
@@ -0,0 +1,6 @@
+var a = ["Dad", "Mom", 3, false];
+// a.push("Balaji");
+// a.push(5);
+// a.push(true);
+// a.push("Hanuman");
+a.forEach(function (val) { return console.log(val); });
---
+++ Day1/Typescript_Basics/Arrays_Different_Types.ts
@@ -0,0 +1,7 @@
+let a:readonly any[] = ["Dad","Mom",3,false];
+// a.push("Balaji");
+// a.push(5);
+// a.push(true);
+// a.push("Hanuman");
+
+a.forEach((val)=>console.log(val));
