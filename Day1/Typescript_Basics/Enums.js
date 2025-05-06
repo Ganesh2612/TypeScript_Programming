d665572e46d5ab1b5ec4fb320047121e3ed26763
Ganesh <ganeshgopi2612@gmail.com>
2025-05-06 14:37:14 +0530

    [Ganesh] add : Implementing Enums in ts

---
+++ Day1/Typescript_Basics/Enums.js
@@ -0,0 +1,9 @@
+var Directions;
+(function (Directions) {
+    Directions[Directions["North"] = 0] = "North";
+    Directions["East"] = "jkbjkbjb";
+    Directions["West"] = "knki";
+    Directions[Directions["South"] = 9] = "South";
+})(Directions || (Directions = {}));
+console.log(Directions.West);
+console.log(Directions.East);
---
+++ Day1/Typescript_Basics/Enums.ts
@@ -0,0 +1,8 @@
+enum Directions{
+    North=0,
+    East="jkbjkbjb",
+    West="knki",
+    South=9
+}
+console.log(Directions.West);
+console.log(Directions.East);
