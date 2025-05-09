dac557f228c4ef507285ddf84cd18ec47e1d3cbb
Ganesh <ganeshgopi2612@gmail.com>
2025-05-09 10:49:53 +0530

    [Ganesh] add : changes done in Read and Write file

--- Day2/File IO/Read_and_write.js
+++ Day2/File IO/Read_and_write.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// file-io.ts
-var fs = require("fs");
-// WRITE to a file (sync)
+
+let fs = require("fs");
 fs.writeFileSync('output.txt', 'Hello from TypeScript!');
-// READ from a file (sync)
-var data = fs.readFileSync('output.txt', 'utf-8');
+
+let data = fs.readFileSync('output.txt', 'utf-8');
 console.log("File Content:", data);
-// APPEND to a file (sync)
+
 fs.appendFileSync('output.txt', '\nAppended Line');
